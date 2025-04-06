afffc3c0c60d327f5e35a9c4a15577f48308c1fc
felipozito <felipozito@hotmail.com>
2025-04-06 16:15:36 -0500

    Reorganizacion de carpetas y curso pandas

--- Pruebas/SistemaSolar/star.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from celestial_object import CelestialObject
-class Star(CelestialObject):
-    def __init__(self, image_path, mass, nucleo_status,):
-        super().__init__(image_path=image_path, mass=mass,)
-        self.nucleo_status=nucleo_status
-        
-    def draw(self, screen):
-        pass
