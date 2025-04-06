2b41bd807ebad8827535c07a85cfb5b020f14a7b
felipozito <felipozito@hotmail.com>
2025-04-06 16:21:18 -0500

    Reorganizacion de archivos

--- 3.Avanzado/Ejercicio POO/celestial_object.py
+++
@@ -1,8 +0,0 @@
-from .celestial_object import CelestialObject
-
-class CelestialObject(CelestialObject):
-    def __init__(self, images,mass, distances=0, orbit_speed=0,):
-        self.images = images
-        self.distance = distance
-        self.orbit_speed = orbit_speed
-        self.mass = mass
--- 3.Avanzado/Ejercicio POO/start.py
+++
@@ -1,4 +0,0 @@
-from celestial_object import CelestialObject
-class Start(CelestialObject):
-    def __init__(self, images, nucleo_status, orbit_speed, mass):
-        super().__init__(images=images, mass=mass)
